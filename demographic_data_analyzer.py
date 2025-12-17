a59b328e63df0c1cbfbb6cfc7696abf343ed40a5
Supravisor <153783117+Supravisor@users.noreply.github.com>
2025-12-17 14:22:05 +1300

    percentage of rich who work minimum hours

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -34,7 +34,7 @@ def calculate_demographic_data(print_data=True):
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
     num_min_workers = df[df['hours-per-week']==min_work_hours]
 
-    rich_percentage = None
+    rich_percentage = round(num_min_workers[num_min_workers.salary=='>50K'].size / num_min_workers.size * 100, 1)
 
     # What country has the highest percentage of people that earn >50K?
     highest_earning_country = None
