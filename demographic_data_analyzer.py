319c9fc9d1fc2c136e55dd1f47da4114f178893f
Supravisor <153783117+Supravisor@users.noreply.github.com>
2025-12-17 14:21:26 +1300

    number of people who work minimum hours

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -32,7 +32,7 @@ def calculate_demographic_data(print_data=True):
     min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    num_min_workers = df[df['hours-per-week']==min_work_hours]
 
     rich_percentage = None
 
